bfb83a02426616b366c8682378c47fa7ac173872
Josthin <jpilcof@unsa.edu.pe>
2021-07-14 05:47:34 -0500

    Se implemento un javaBean y se hizo uso de la librería JSTL

---
+++ src/main/java/net/codejava/javaee/Person.java
@@ -0,0 +1,23 @@
+package net.codejava.javaee;
+
+public class Person {
+	private String name;
+	private Integer edad;
+	
+	public Person() {
+		
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getEdad() {
+		return edad;
+	}
+	public void setEdad(Integer edad) {
+		this.edad = edad;
+	}
+	
+}
